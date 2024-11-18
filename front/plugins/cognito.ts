64aa3dae36517473506e6bd94d06357fdaa8cfcc
Yosuke Maruyama <yosuke.maruyama@whitecanvas.jp>
2024-11-18 16:27:01 +0900

    global value

---
+++ front/plugins/cognito.ts
@@ -0,0 +1,19 @@
+// plugins/cognito.ts
+
+import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import { defineNuxtPlugin } from '#app';
+
+/**
+ * nuxtアプリ全体でuserPoolを利用するための設定
+ */
+export default defineNuxtPlugin((nuxtApp) => {
+    const runtimeConfig = useRuntimeConfig();
+
+    const poolData = {
+        UserPoolId: runtimeConfig.public.userPoolId as string,
+        ClientId: runtimeConfig.public.clientId as string,
+    };
+
+    const userPool = new CognitoUserPool(poolData);
+    nuxtApp.provide('userPool', userPool);
+});
