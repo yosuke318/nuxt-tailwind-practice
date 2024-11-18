75eeb88b905a9beeaff2e8039daa0b3c6f9a7c33
Yosuke Maruyama <yosuke.maruyama@whitecanvas.jp>
2024-11-18 16:25:34 +0900

    cahge environment value

--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss']
-})
+  modules: ['@nuxtjs/tailwindcss'],
+  vite: {
+    define: {
+      'window.global': {}, // In SSR, window is not defined. This is a workaround.
+    },
+  },
+  runtimeConfig: {
+    public: {
+      userPoolId: process.env.NUXT_PUBLIC_USER_POOL_ID,
+      clientId: process.env.NUXT_PUBLIC_CLIENT_ID,
+    }
+  },
+});
