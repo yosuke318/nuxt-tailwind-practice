42cc4729c4270a4866b98ef8a3899e49ce7f783d
Yosuke Maruyama <yosuke.maruyama@whitecanvas.jp>
2024-11-19 14:07:49 +0900

    signup sample

---
+++ front/pages/signup-sample.vue
@@ -0,0 +1,145 @@
+<!-- index.vue -->
+<template>
+  <div class="h-screen bg-[#f8f8f9] flex justify-center items-center">
+
+
+    <div class="w-[400px] py-6 bg-white rounded-lg flex-col justify-start items-start gap-6 inline-flex">
+      <div class="self-stretch h-[34.29px] px-6 flex-col justify-start items-center gap-1.5 flex">
+        <div class="justify-start items-end gap-6 inline-flex">
+          <img class="w-[120px] h-[34.29px]" src="../public/favicon.ico" alt="morimori Icon"/>
+          <div class="text-[#727785] text-sm font-bold font-['Inter'] leading-tight tracking-wide">
+            For Example
+          </div>
+        </div>
+      </div>
+
+<!--ボーダー部分-->
+      <div class="self-stretch h-px flex-col justify-center items-start flex">
+        <div class="self-stretch h-[0px] border border-[#e1e2ec]"></div>
+      </div>
+
+<!--メールアドレス部分      -->
+      <div class="self-stretch h-[390px] px-6 flex-col justify-start items-start gap-6 flex">
+        <div class="self-stretch h-[45px] flex-col justify-center items-start gap-2 flex">
+          <input placeholder="メールアドレス" class="self-stretch h-[45px] px-3 py-2.5 bg-white rounded border border-[#c2c6d6] justify-end items-center gap-2 inline-flex">
+        </div>
+
+
+<!--氏名        -->
+        <div class="self-stretch h-[69px] flex-col justify-start items-start gap-2 flex">
+          <div class="self-stretch justify-start items-center gap-2 inline-flex">
+            <div class="text-[#424753] text-xs font-medium font-['Inter'] leading-none tracking-wide">
+              氏名
+            </div>
+          </div>
+          <div class="self-stretch h-[45px] px-3 py-2.5 bg-white rounded border border-[#c2c6d6]"></div>
+        </div>
+
+<!--パスワード   -->
+        <div class="self-stretch h-[69px] flex-col justify-start items-start gap-2 flex">
+          <div class="self-stretch justify-start items-center gap-2 inline-flex">
+            <div class="text-[#424753] text-xs font-medium font-['Inter'] leading-none tracking-wide">
+              パスワード
+            </div>
+          </div>
+
+          <div
+              class="self-stretch h-[50px] px-3 py-2.5
+              bg-white rounded border border-[#c2c6d6]
+              justify-end items-center gap-2 inline-flex input-container">
+            <input :type="showPassword ? 'text' : 'password'"
+                   v-model="password"
+                   class="w-full focus:outline-none"
+            >
+
+            <div class="w-6 h-6 relative">
+                          <span
+                              @click="togglePasswordVisibility"
+                              class="tw-absolute tw-inset-y-0 tw-right-0 tw-pr-3 tw-flex tw-items-center"
+                              type="button"
+                          >
+                            <span v-if="showPassword" class="tw-text-gray-600">
+                              <img src="../public/open_eye.png" alt="view password">
+                            </span>
+                            <span v-else class="tw-text-gray-600">
+                              <img src="../public/closed_eye.png" alt="not view password">
+                            </span>
+                          </span>
+            </div>
+          </div>
+        </div>
+        <div class="self-stretch h-[69px] flex-col justify-start items-start gap-2 flex">
+          <div class="self-stretch justify-start items-center gap-2 inline-flex">
+            <div class="text-[#424753] text-xs font-medium font-['Inter'] leading-none tracking-wide">
+              パスワード確認
+            </div>
+          </div>
+
+          <div
+              class="self-stretch h-[50px] px-3 py-2.5
+              bg-white rounded border border-[#c2c6d6]
+              justify-end items-center gap-2 inline-flex input-container">
+            <input :type="showPasswordVerify ? 'text' : 'password'"
+                   v-model="passwordVerify"
+                   class="w-full focus:outline-none"
+            >
+
+            <div class="w-6 h-6 relative">
+                          <span
+                              @click="togglePasswordVerifyVisibility"
+                              class="tw-absolute tw-inset-y-0 tw-right-0 tw-pr-3 tw-flex tw-items-center"
+                              type="button"
+                          >
+                            <span v-if="showPasswordVerify" class="tw-text-gray-600">
+                              <img src="../public/open_eye.png" alt="view password">
+                            </span>
+                            <span v-else class="tw-text-gray-600">
+                              <img src="../public/closed_eye.png" alt="not view password">
+                            </span>
+                          </span>
+            </div>
+          </div>
+        </div>
+
+
+
+      </div>
+
+
+
+
+      <div class="self-stretch h-[42px] bg-[#2271e3] rounded-lg justify-center items-center inline-flex">
+        <div class="grow shrink basis-0 h-10 px-4 py-3 justify-center items-center gap-2.5 flex">
+          <div class="text-right text-white text-xs font-bold font-['Inter'] leading-none tracking-wide">登録
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import {ref, computed} from 'vue'
+
+const password = ref('')
+const passwordVerify = ref('')
+const showPassword = ref(false)
+const showPasswordVerify = ref(false)
+
+const togglePasswordVisibility = () => {
+  showPassword.value = !showPassword.value
+}
+
+const togglePasswordVerifyVisibility = () => {
+  showPasswordVerify.value = !showPasswordVerify.value
+}
+
+</script>
+
+<style>
+.input-container:focus-within {
+  border-color: blue;
+  transition: border-color 0.3s ease;
+}
+</style>
