1b88f539153c60314548762c8d3acae9bd7a6b78
Yosuke Maruyama <yosuke.maruyama@whitecanvas.jp>
2024-11-19 12:15:35 +0900

    signup example

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -2,6 +2,8 @@
 <template>
   <h1>Landing Page</h1>
   <nuxt-link to="/login">Login</nuxt-link>
+  <h1>サインアップ</h1>
+  <nuxt-link to="/signup-sample">signup sample</nuxt-link>
 </template>
 
 <script setup lang="ts">
