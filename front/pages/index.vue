d6683c209ee6ebde7572a03fb4a5bea3383fc826
Yosuke Maruyama <yosuke.maruyama@whitecanvas.jp>
2024-11-20 17:53:22 +0900

    wip new mail address

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -8,6 +8,9 @@
   <nuxt-link to="/login-sample">login sample</nuxt-link>
   <h1>新しいパスワード</h1>
   <nuxt-link to="/new-password">newpassword sample</nuxt-link>
+
+  <h1>新しいメールアドレス</h1>
+  <nuxt-link to="/new-mail">new mail address sample</nuxt-link>
   <h1>テスト</h1>
   <nuxt-link to="/text">test</nuxt-link>
 </template>
