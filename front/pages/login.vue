1eb0e134772b08a6c225e4e017f17fcba8d70d06
Yosuke Maruyama <yosuke.maruyama@whitecanvas.jp>
2024-11-22 09:04:54 +0900

    signup confirm

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -9,7 +9,7 @@
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import {ref} from 'vue';
 import {CognitoUser, AuthenticationDetails} from 'amazon-cognito-identity-js';
 
--- front/pages/signup.vue
+++ front/pages/signup.vue
@@ -1,4 +1,14 @@
-<script setup lang="ts">
+<template>
+  <div>
+    <h1>Sign Up</h1>
+    <input v-model="email" placeholder="Email" />
+    <input type="password" v-model="password" placeholder="Password" />
+    <button @click="signup">sing up</button>
+  </div>
+
+</template>
+
+<script setup>
 import {ref} from "vue";
 import {useRouter} from "vue-router";
 import {CognitoUserAttribute} from "amazon-cognito-identity-js"
@@ -10,11 +20,30 @@ const email = ref("");
 const password = ref("");
 const router = useRouter();
 
+const signup = () =>  {
+  const attributeList =[];
 
+  const dataEmail = {
+    Name: 'email',
+    Value: email.value
+  };
+
+  // cognitoで設定した属性
+  const attributeEmail = new CognitoUserAttribute(dataEmail)
+
+  attributeList.push(attributeEmail)
+
+  userPool.signUp(email.value, password.value, attributeList, null, (err, result) => {
+    if (err) {
+      alert(err.message || JSON.stringify(err));
+      return;
+    }
+    const cognitoUser = result.user;
+    console.log('user name is ' + cognitoUser.getUsername());
+
+    router.push({ name: 'signup-confirm', query: { email: email.value } });
+  });
+
+}
 
 </script>
-
-<template>
-
-</template>
-
